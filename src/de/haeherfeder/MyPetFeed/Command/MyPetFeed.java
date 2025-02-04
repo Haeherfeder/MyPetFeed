3bdbd4002868154ce55f3017094727b79043c571
haeherfeder <guthub@haeherfeder.de>
2025-02-05 00:30:00 +0100

    bug fixes

--- src/de/haeherfeder/MyPetFeed/Command/MyPetFeed.java
+++ src/de/haeherfeder/MyPetFeed/Command/MyPetFeed.java
@@ -9,14 +9,14 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+
 public class MyPetFeed implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if(!(command instanceof Player)){
+        if(!(commandSender instanceof Player p)){
             commandSender.sendMessage("only possible as Player");
             return false;
         }
-        Player p = (Player) commandSender;
         MyPetPlayer mp = MyPetApi.getPlayerManager().getMyPetPlayer(p);
         if(mp == null){
             p.sendMessage("Couldn't get Mypet Player");
@@ -27,12 +27,12 @@ public class MyPetFeed implements CommandExecutor {
             p.sendMessage("you dont have a pet.");
             return false;
         }
-        System.out.println(MyPetApi.getMyPetInfo().getFood(pet.getPetType()));
+//        System.out.println(MyPetApi.getMyPetInfo().getFood(pet.getPetType()));
         if(!pet.getPetType().equals(MyPetType.EnderDragon)){
             p.sendMessage("Your pet is not an Enderdragon. please feed it yourself");
             return true;
         }
-        System.out.println(pet.getSaturation());
+//        System.out.println(pet.getSaturation());
         pet.setSaturation(100);
         return true;
     }
